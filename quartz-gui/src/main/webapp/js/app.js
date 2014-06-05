2ba946bebd9a3bf4d8bae37bf6abb41e349124dc
Seth Wingert <sethwingert@yahoo.com>
2014-06-05 10:44:26 -0700

    Set up first GUI screen!

--- quartz-gui/src/main/webapp/js/app.js
+++ quartz-gui/src/main/webapp/js/app.js
@@ -10,6 +10,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/newServerConfig', {templateUrl: 'partials/newServerConfig.html', controller: 'ServerConfigController'});
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
